f6f423cf8dcc05e6fbd42e64e5da22f6da4782ed
Opilane TTHK <opilane@tthk.ee>
2021-02-03 15:39:34 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace CinemaSeats
+{
+    public partial class Form1 : Form
+    {
+        public Button small, medium, big;
+        public Form1()
+        {
+            Height = 300;
+            Width = 300;
+            Text = "Cinema";
+            small = new Button
+            {
+                Size = new Size(80, 50),
+                Location = new Point(100, 30),
+                Text = "Small"
+            };
+            medium = new Button
+            {
+                Size = new Size(80, 50),
+                Location = new Point(100, 85),
+                Text = "Medium"
+            };
+            big = new Button
+            {
+                Size = new Size(80, 50),
+                Location = new Point(100, 140),
+                Text = "Big"
+            };
+            this.Controls.Add(small);
+            this.Controls.Add(medium);
+            this.Controls.Add(big);
+        }
+
+
+
+    }
+}
